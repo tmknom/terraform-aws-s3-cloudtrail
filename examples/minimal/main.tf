cea0f9ccaf6eef7fcb9d83d748dd92a13f1ecb26
tmknom <tmk.nom@gmail.com>
2018-10-26 00:44:42 +0900

    Implement minimal example

--- examples/minimal/main.tf
+++ examples/minimal/main.tf
@@ -1,3 +1,9 @@
 # Module Configuration
 #
 # https://www.terraform.io/docs/configuration/modules.html
+
+module "s3_cloudtrail" {
+  source                = "../../"
+  name                  = "s3-cloudtrail"
+  logging_target_bucket = "s3-access-log"
+}
--- examples/minimal/outputs.tf
+++ examples/minimal/outputs.tf
@@ -1,3 +1,23 @@
 # Output Configuration
 #
 # https://www.terrafsorm.io/docs/configuration/outputs.html
+
+output "s3_bucket_id" {
+  value = "${module.s3_cloudtrail.s3_bucket_id}"
+}
+
+output "s3_bucket_arn" {
+  value = "${module.s3_cloudtrail.s3_bucket_arn}"
+}
+
+output "s3_bucket_domain_name" {
+  value = "${module.s3_cloudtrail.s3_bucket_domain_name}"
+}
+
+output "s3_bucket_hosted_zone_id" {
+  value = "${module.s3_cloudtrail.s3_bucket_hosted_zone_id}"
+}
+
+output "s3_bucket_region" {
+  value = "${module.s3_cloudtrail.s3_bucket_region}"
+}

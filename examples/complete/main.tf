a7c34295b1368492ebe693dc98293c9276b40839
tmknom <tmk.nom@gmail.com>
2018-10-26 00:46:27 +0900

    Implement complete example

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -1,3 +1,25 @@
 # Module Configuration
 #
 # https://www.terraform.io/docs/configuration/modules.html
+
+module "s3_cloudtrail" {
+  source                = "../../"
+  name                  = "s3-cloudtrail"
+  logging_target_bucket = "s3-access-log"
+
+  versioning_enabled = false
+  force_destroy      = true
+
+  lifecycle_rule_enabled                     = true
+  lifecycle_rule_prefix                      = ""
+  standard_ia_transition_days                = "60"
+  glacier_transition_days                    = "90"
+  expiration_days                            = "180"
+  glacier_noncurrent_version_transition_days = "60"
+  noncurrent_version_expiration_days         = "90"
+
+  tags = {
+    Environment = "prod"
+    Name        = "s3-access-log"
+  }
+}

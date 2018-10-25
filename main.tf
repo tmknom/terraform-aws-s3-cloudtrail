49053e27a3f26f15fc03c89c914195248f78dc40
tmknom <tmk.nom@gmail.com>
2018-10-26 00:21:54 +0900

    Add versioning enabled

--- main.tf
+++ main.tf
@@ -17,4 +17,14 @@ resource "aws_s3_bucket" "default" {
   # S3 access control lists (ACLs) enable you to manage access to buckets and objects.
   # https://docs.aws.amazon.com/AmazonS3/latest/dev/acl-overview.html
   acl = "private"
+
+  # Versioning is a means of keeping multiple variants of an object in the same bucket.
+  # Versioning-enabled buckets enable you to recover objects from accidental deletion or overwrite.
+  #
+  # Once you version-enable a bucket, it can never return to an unversioned state.
+  # You can, however, suspend versioning on that bucket.
+  # https://docs.aws.amazon.com/AmazonS3/latest/dev/Versioning.html
+  versioning {
+    enabled = "${var.versioning_enabled}"
+  }
 }
--- variables.tf
+++ variables.tf
@@ -6,3 +6,9 @@ variable "name" {
   type        = "string"
   description = "The name of the bucket, which must comply with DNS naming conventions."
 }
+
+variable "versioning_enabled" {
+  default     = true
+  type        = "string"
+  description = "Enable versioning. Versioning is a means of keeping multiple variants of an object in the same bucket."
+}
